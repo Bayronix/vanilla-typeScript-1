b2699c115d170f0deaaa5fbaf81cda9ac13690c0
Bayronix <151779642+Bayronix@users.noreply.github.com>
2024-08-15 11:41:31 +0200

    comm

--- src/main.ts
+++ src/main.ts
@@ -193,3 +193,20 @@
 // }
 
 // massive.forEach((user) => console.log(age(user)));
+
+interface Person {
+  firstName: string;
+  lastName: string;
+  age?: number; // Необов'язкове поле
+}
+
+function greet(person: Person) {
+  console.log(`Hello, ${person.firstName} ${person.lastName} `);
+}
+
+const john: Person = {
+  firstName: "John",
+  lastName: "Doe",
+};
+
+greet(john); // Виведе: "Hello, John Doe"
